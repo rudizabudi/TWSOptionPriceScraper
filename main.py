b115e4bfce1d1ebc26663656cdd5fb00b445cf43
rudizabudi <40042979+rudizabudi@users.noreply.github.com>
2025-05-05 15:12:40 +0200

    start API with script

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ from tws_api import TWSCon
 if __name__ == '__main__':
     def main():
         core: Core = Core()
+
+        if core.USE_IBC:
+            start_ibgateway(core)
+
         tws_con: TWSCon = TWSCon(core=core)
 
         while not core.tws_con:
@@ -22,7 +26,6 @@ if __name__ == '__main__':
             sleep(1)
 
         sleep(60)
-
         while True:
             if not tws_con.isConnected():
                 tws_con = TWSCon(core=core)
@@ -34,9 +37,7 @@ if __name__ == '__main__':
                     tprint(f'TWS API might not send data any longer.')
                     if core.USE_IBC:
                         kill_ibgateway(core)
-                        sleep(30)
                         start_ibgateway(core)
-                        sleep(30)
             except TypeError:
                 pass
 
